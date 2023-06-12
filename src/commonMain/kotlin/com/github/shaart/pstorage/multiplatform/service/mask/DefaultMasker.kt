7565463a208ae7e9737423f38817c1014cccaf3e
Artur Shalaev <imshaart@gmail.com>
2023-06-12 20:54:54 +0300

    Fix index out of bound for DefaultMasker

--- src/commonMain/kotlin/com/github/shaart/pstorage/multiplatform/service/mask/DefaultMasker.kt
+++ src/commonMain/kotlin/com/github/shaart/pstorage/multiplatform/service/mask/DefaultMasker.kt
@@ -2,6 +2,9 @@ package com.github.shaart.pstorage.multiplatform.service.mask
 
 class DefaultMasker : Masker {
     override fun username(login: String): String {
+        if (login.isEmpty()) {
+            return login
+        }
         if (login.length > 5) {
             return login.substring(0, 3).plus("*".repeat(5))
         }
@@ -9,6 +12,9 @@ class DefaultMasker : Masker {
     }
 
     override fun alias(alias: String): String {
+        if (alias.isEmpty()) {
+            return alias
+        }
         if (alias.length > 3) {
             return alias.substring(0, 2).plus("*".repeat(6))
         }
